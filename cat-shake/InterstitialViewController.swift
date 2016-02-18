ee3ece77581cabf5289a6b419dd3cf4fe0b2e69b
Shirley He <she@wesleyan.edu>
2016-02-17 21:01:46 -0500

    added fixes, randomization attempt

--- cat-shake/InterstitialViewController.swift
+++ cat-shake/InterstitialViewController.swift
@@ -6,19 +6,26 @@
 //  Created by Mack on 1/31/16.
 //  Copyright © 2016 Mack. All rights reserved.
 //
+
 import UIKit
 
 class InterstitialViewController: UIViewController {
-    var catGifArray = [UIImage]()
+    //var catGifArray = [UIImage]()
+    
     let gifView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // view.backgroundColor = UIColor.redColor()
         setUpGifView()
-        self.checkAnimationImages(self.catGifName)
-        //animateImage()
+       // self.checkAnimationImages(self.catGifName)
+      //animateImage()
     }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        playRandomGif()
+    }
+    
     func setUpGifView() {
         view.addSubview(gifView)
         gifView.snp_makeConstraints { (make) -> Void in
@@ -66,24 +73,29 @@ class InterstitialViewController: UIViewController {
         "desk",
         "waving",
     ]
-    
-    func checkAnimationImages(fileNames: [String]) -> [AnyObject] {
-        for name in fileNames {
-            var i = 0
-            //var catGifArray = [UIImage]()
-            var image = UIImage(named: "\(name)-\(i)")
-            while (image != nil){
-                catGifArray.append(image!)
-                ++i
-                image = UIImage(named: "\(name)-\(i)")
-            }
+    func gifAnimationImages(fileName: String) -> [UIImage] {
+        var gifArray = [UIImage]()
+        var i = 0
+        var image = UIImage(named: "\(fileName)-\(i)")
+        while (image != nil) {
+            gifArray.append(image!)
+            ++i
+            image = UIImage(named: "\(fileName)-\(i)")
         }
-        self.gifView.animationImages = catGifArray;
+        return gifArray
+    }
+    
+    func playRandomGif() {
+        // select random element from catGifNameArray
+        //let randomElement = catGifName.first
+        
+        let randomElement = Int(arc4random_uniform(UInt32(catGifName.count)))
+        var result = self.catGifName[randomElement]
+        let images = gifAnimationImages(result)
+      
+        self.gifView.animationImages = images;
         self.gifView.animationDuration = 1.0
         self.gifView.startAnimating()
-        
-        return catGifArray.map {
-            return $0.CGImage as! AnyObject
-        }
     }
+    
 }
